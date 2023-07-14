1897b688bb58508bb8b85dfe2990d3db273f58fd
seumomo <xo0207@naver.com>
2023-07-14 16:06:00 +0900

    기본 구현 완료 및 새로고침 시 input 초기화 되도록 코드 추가

--- mission01/js/main.js
+++ mission01/js/main.js
@@ -17,9 +17,9 @@ function passwordReg(text) {
   return re.test(String(text).toLowerCase());
 }
 
-/* -------------------------------------------------------------------------- */
-/*                               input 요소 찾기 함수                         */
-/* -------------------------------------------------------------------------- */
+/* -------------------- */
+/* input 요소 찾기 함수 */
+/* -------------------- */
 function getInput(type) {
   const input = document.querySelector(`[type = ${type}]`);
 
@@ -30,9 +30,21 @@ function getInput(type) {
   }
 }
 
-/* -------------------------------------------------------------------------- */
-/*                     input 요소에 입력되는 값을 validation하는 함수         */
-/* -------------------------------------------------------------------------- */
+/* --------------------------------------------------------------------------------- */
+/* 페이지 이동 후 뒤로가기를 했을 때 모든 input의 value와 check를 초기화되도록 한다. */
+/* --------------------------------------------------------------------------------- */
+window.addEventListener('pageshow', () => {
+  const allInput = document.querySelectorAll('input');
+
+  for (let i = 0; i < allInput.length; i++) {
+    allInput[i].value = null;
+    allInput[i].checked = false;
+  }
+});
+
+/* ---------------------------------------------- */
+/* input 요소에 입력되는 값을 validation하는 함수 */
+/* ---------------------------------------------- */
 function validation(type) {
   const input = getInput(`${type}`);
   let value = input.value;
@@ -42,10 +54,10 @@ function validation(type) {
     password: passwordReg,
   };
   const validationFunction = validationFunctions[type];
+  let isInvalid = false;
 
   input.addEventListener('input', () => {
     value = input.value;
-    let isInvalid = false;
 
     if (value) {
       isInvalid = !validationFunction(value);
@@ -54,6 +66,7 @@ function validation(type) {
     input.classList.toggle('is--invalid', isInvalid);
   });
 }
+
 /* ------------------------------------------------------------------------------------------------ */
 /*               해당하는 input 요소를 찾고, 그 요소에 입력된 값을 validation하는 함수              */
 /* ------------------------------------------------------------------------------------------------ */
@@ -69,22 +82,30 @@ inputValidation('password');
 
 // 3. 상태 변수 관리
 // 4. 로그인 버튼을 클릭시 조건처리
-function checkInput(type) {
-  const input = getInput(`${type}`);
-  let value = input.value;
-
-  input.addEventListener('input', () => {
-    value = input.value;
-
-    if (value === user[`${type}`]) {
-      return console.log(true);
-    } else {
-      console.log(`${type}이(가) 옳지 않습니다.`);
-    }
-  });
-}
-const checkEmail = checkInput('email');
-const checkPassword = checkInput('password');
-if (checkEmail === true && checkPassword === true) {
-  window.location.href = 'welcome.html';
-}
+// const email = getInput('email');
+// const password = getInput('password');
+// let emailValue = email.value;
+// let passwordValue = password.value;
+// email.addEventListener('input', () => {
+//   emailValue = email.value;
+//   if (emailValue === user.email) {
+//     return (emailValue = true);
+//   }
+//   console.log(emailValue);
+// });
+// password.addEventListener('input', () => {
+//   passwordValue = password.value;
+//   if (passwordValue === user.password) {
+//     return (passwordValue = true);
+//   }
+//   console.log(user.password);
+// });
+// const form = document.querySelector('form');
+// form.addEventListener('submit', () => {
+//   event.preventDefault();
+//   if (emailValue === true && passwordValue === true) {
+//     window.location.href = 'welcome.html';
+//   } else {
+//     alert('아이디 또는 비밀번호가 옳지 않습니다.');
+//   }
+// });
