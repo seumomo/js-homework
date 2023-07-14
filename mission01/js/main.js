8ae82b3d926d872302b804d5555dc1cb7d59ca8d
seumomo <xo0207@naver.com>
2023-07-14 17:26:43 +0900

    코드 배치 수정 및 뒤로가기 동작 코드를 reset 메소드를 사용하는 것으로 수정

--- mission01/js/main.js
+++ mission01/js/main.js
@@ -1,8 +1,14 @@
+/* --------------------- */
+/*    기본 user 객체     */
+/* --------------------- */
 const user = {
   email: 'asd@naver.com',
   password: 'spdlqj123!@',
 };
 
+/* ---------------------------------------------------------------------- */
+/*                     email과 password의 정규 표현식                     */
+/* ---------------------------------------------------------------------- */
 function emailReg(text) {
   const re =
     // eslint-disable-next-line no-useless-escape
@@ -17,9 +23,9 @@ function passwordReg(text) {
   return re.test(String(text).toLowerCase());
 }
 
-/* -------------------- */
-/* input 요소 찾기 함수 */
-/* -------------------- */
+/* ------------------------------------------------------------ */
+/*                     input 요소 찾기 함수                     */
+/* ------------------------------------------------------------ */
 function getInput(type) {
   const input = document.querySelector(`[type = ${type}]`);
 
@@ -30,23 +36,11 @@ function getInput(type) {
   }
 }
 
-/* --------------------------------------------------------------------------------- */
-/* 페이지 이동 후 뒤로가기를 했을 때 모든 input의 value와 check를 초기화되도록 한다. */
-/* --------------------------------------------------------------------------------- */
-window.addEventListener('pageshow', () => {
-  const allInput = document.querySelectorAll('input');
-
-  for (let i = 0; i < allInput.length; i++) {
-    allInput[i].value = null;
-    allInput[i].checked = false;
-  }
-});
-
-/* ---------------------------------------------- */
-/* input 요소에 입력되는 값을 validation하는 함수 */
-/* ---------------------------------------------- */
+/* -------------------------------------------------- */
+/*   input 요소에 입력되는 값을 validation하는 함수   */
+/* -------------------------------------------------- */
 function validation(type) {
-  const input = getInput(`${type}`);
+  const input = getInput(type);
   let value = input.value;
 
   const validationFunctions = {
@@ -67,45 +61,31 @@ function validation(type) {
   });
 }
 
-/* ------------------------------------------------------------------------------------------------ */
-/*               해당하는 input 요소를 찾고, 그 요소에 입력된 값을 validation하는 함수              */
-/* ------------------------------------------------------------------------------------------------ */
-function inputValidation(type) {
-  getInput(`${type}`);
-  validation(`${type}`);
-}
+/* ------------------- */
+/*      함수 실행      */
+/* ------------------- */
+validation('email');
+validation('password');
 
-// 1. email 정규표현식을 사용한 validation
-// 2. pw 정규표현식을 사용한 validation
-inputValidation('email');
-inputValidation('password');
+/* --------------------------------------------------------------------------------------------------- */
+/* 로그인 버튼을 누르면 user 객체의 값과 비교한 후 일치하면 로그인, 일치하지 않으면 알림창을 띄워준다. */
+/* --------------------------------------------------------------------------------------------------- */
+document.querySelector('form').addEventListener('submit', () => {
+  event.preventDefault();
 
-// 3. 상태 변수 관리
-// 4. 로그인 버튼을 클릭시 조건처리
-// const email = getInput('email');
-// const password = getInput('password');
-// let emailValue = email.value;
-// let passwordValue = password.value;
-// email.addEventListener('input', () => {
-//   emailValue = email.value;
-//   if (emailValue === user.email) {
-//     return (emailValue = true);
-//   }
-//   console.log(emailValue);
-// });
-// password.addEventListener('input', () => {
-//   passwordValue = password.value;
-//   if (passwordValue === user.password) {
-//     return (passwordValue = true);
-//   }
-//   console.log(user.password);
-// });
-// const form = document.querySelector('form');
-// form.addEventListener('submit', () => {
-//   event.preventDefault();
-//   if (emailValue === true && passwordValue === true) {
-//     window.location.href = 'welcome.html';
-//   } else {
-//     alert('아이디 또는 비밀번호가 옳지 않습니다.');
-//   }
-// });
+  if (
+    getInput('email').value === user.email &&
+    getInput('password').value === user.password
+  ) {
+    window.location.href = 'welcome.html';
+  } else {
+    alert('아이디 또는 비밀번호가 옳지 않습니다.');
+  }
+});
+
+/* ----------------------------------------------------------- */
+/* 페이지 이동 후 뒤로가기를 했을 때 form을 초기화되도록 한다. */
+/* ----------------------------------------------------------- */
+window.addEventListener('pageshow', () => {
+  document.querySelector('form').reset();
+});
