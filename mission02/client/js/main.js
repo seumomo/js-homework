6723e9cacbe9e7a2d8707ada16fb0d43ef723acf
seumomo <xo0207@naver.com>
2023-07-20 08:37:43 +0900

    slider 함수 생성 및 ul에 클래스 추가

--- mission02/client/js/main.js
+++ mission02/client/js/main.js
@@ -1,4 +1,3 @@
-
 /* 
 
 1. 클릭 이벤트 활성화
@@ -9,13 +8,26 @@
 
 */
 
+const list = document.querySelector('.nav__list');
+const visualImage = document.querySelector('.visual img');
 
+function slider(event) {
+  const target = event.target.closest('li');
 
+  if (!target) return;
 
+  for (let value of list.children) {
+    value.classList.remove('is-active');
+  }
 
+  target.classList.add('is-active');
 
+  setImage();
+}
 
+function setImage(event) {
+  // visualImage.setAttribute('src', `./assets/${data[name].toLowerCase.jpeg}`);
+  console.log(data[index]);
+}
 
-
-
-
+list.addEventListener('click', slider);
