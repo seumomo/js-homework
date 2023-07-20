bcec2e42052685ca9b3f7d4e26a9e8508e33bb3a
seumomo <xo0207@naver.com>
2023-07-20 16:38:44 +0900

    과제 구현 완료

--- mission02/client/js/main.js
+++ mission02/client/js/main.js
@@ -9,12 +9,15 @@
 */
 
 const list = document.querySelector('.nav__list');
+const li = document.querySelectorAll('li');
 const visualImage = document.querySelector('.visual img');
 
 function slider(event) {
   const target = event.target.closest('li');
-
   if (!target) return;
+
+  const index = target.getAttribute('data-index') - 1;
+  const imageName = data[index].name.toLowerCase();
 
   for (let value of list.children) {
     value.classList.remove('is-active');
@@ -22,12 +25,14 @@ function slider(event) {
 
   target.classList.add('is-active');
 
-  setImage();
-}
+  visualImage.setAttribute('src', `./assets/${imageName}.jpeg`);
+  visualImage.setAttribute('alt', `./assets/${data[index].alt}`);
 
-function setImage(event) {
-  // visualImage.setAttribute('src', `./assets/${data[name].toLowerCase.jpeg}`);
-  console.log(data[index]);
+  const body = document.querySelector('body');
+  body.style.background = `linear-gradient(to bottom, ${data[index].color[0]}, ${data[index].color[1]})`;
+
+  let nickName = document.querySelector('.nickName');
+  nickName.innerHTML = data[index].name;
 }
 
 list.addEventListener('click', slider);
